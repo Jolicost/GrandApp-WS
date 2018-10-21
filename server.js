6cb38e544de11987cf9577c2c65e584b64ce69bb
Jolicost <costasca6@gmail.com>
2018-10-21 15:33:56 +0200

    switch db url in production

--- server.js
+++ server.js
@@ -6,7 +6,17 @@ var express = require('express'),
     bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/ActivityDB');
+var production = 'mongodb://heroku_xwn18t95:bojkqnjfvltgf6bdcakcqd5aga@ds137643.mlab.com:37643/heroku_xwn18t95'
+var local = 'mongodb://localhost/Grandapp';
+var dbURL = production;
+
+const environment = process.env.NODE_ENV || 'development';
+
+if (environment === 'development' ) {
+    dbURL = local;
+}
+
+mongoose.connect(dbURL);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());

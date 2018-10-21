ae4bfe9fae8d9fde4253966cd85e5e5d110022a5
Jolicost <costasca6@gmail.com>
2018-10-21 22:55:24 +0200

    allow cross-site requests

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ mongoose.connect(dbURL);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 var routes = require('./api/routes/activityRoutes');
 routes(app);
 

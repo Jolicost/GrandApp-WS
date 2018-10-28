075b5ae46d563d0722bc603e9cd3c2142964da84
Jolicost <costasca6@gmail.com>
2018-10-28 22:24:06 +0100

    config files

---
+++ config/config.js
@@ -0,0 +1,35 @@
+const convict = require('convict');
+
+// Define schema
+var defaultPort = process.env.PORT || 3000;
+
+var config = convict({
+  env: {
+    doc: "Grandapp Application Environment",
+    format: ["production", "development", "test"],
+    default: "development",
+    env: "NODE_ENV"
+  },  
+  db: {
+    host: {
+      doc: "Database host name/IP",
+      format: '*',
+      default: 'mongodb://localhost/ASW'
+    }
+  },
+  server: {
+    port: {
+      doc: "Server PORT",
+      format: '*',
+      default: defaultPort
+    }  
+  } 
+});
+
+// Load environment dependent configuration
+var env = config.get('env');
+config.loadFile('./config/' + env + '.json');
+
+// Perform validation
+config.validate({allowed: 'strict'});
+module.exports = config;
--- server.js
+++ server.js
@@ -1,23 +1,24 @@
+// Hey buddy let's see how this goes
+console.log('Reading configuration files...');
+const config = require('./config/config.js');
+// Read the configuration variables
+var PORT  = config.get('server.port');
+var DB = config.get('db.host');
+// Print some of them just for information
+console.log('Read PORT from config: ' + PORT);
+console.log('Read Database host DB from config: ' + DB);
+/* express routing dependency. Add all your modules here */
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000,
+    port = PORT,
     mongoose = require('mongoose'),
     Activity = require('./api/models/activityModel'),
     //ActivityList = require('./api/models/activityListModel'),
     bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-var production = 'mongodb://heroku_xwn18t95:bojkqnjfvltgf6bdcakcqd5aga@ds137643.mlab.com:37643/heroku_xwn18t95'
-var local = 'mongodb://localhost/Grandapp';
-var dbURL = production;
 
-const environment = process.env.NODE_ENV || 'development';
-
-if (environment === 'development' ) {
-    dbURL = local;
-}
-
-mongoose.connect(dbURL,{ 
+mongoose.connect(DB,{ 
     useNewUrlParser: true 
 });
 
@@ -34,6 +35,6 @@ app.use(function(req, res, next) {
 var routes = require('./api/routes/activityRoutes');
 routes(app);
 
-app.listen(port);
+app.listen(PORT);
 
 console.log('Grandapp RESTful API server started on: ' + port);

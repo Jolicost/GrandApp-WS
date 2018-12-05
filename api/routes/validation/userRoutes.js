bbb50f7fc2d79d71a8d9e147ee21bb8f3171f17d
Jolicost <costasca6@gmail.com>
2018-12-05 09:20:28 +0100

    bug fix on validate edit

--- api/routes/validation/userRoutes.js
+++ api/routes/validation/userRoutes.js
@@ -5,7 +5,6 @@ module.exports = {
   updateNormal: {
     body: {
       completeName: Joi.string().required(),
-      address: Joi.string().required(),
       email: Joi.string().required(),
       phone: Joi.string().required(),
       profilePic: Joi.string().required()

9a6eb19e55bc58ca1b5e99d108e400e0ba0f9049
Jolicost <costasca6@gmail.com>
2019-01-04 17:34:30 +0100

    presentation data E

--- presentation/e/3-users/users.js
+++ presentation/e/3-users/users.js
@@ -4,6 +4,8 @@ const {
 
 const bcrypt = require('bcryptjs');
 var passwordHash = require('password-hash');
+var _ = require("underscore");
+var maxUsers = 1000;
 
 function hashPassword(password) {
     return passwordHash.generate(password);
@@ -32,5 +34,45 @@ const users = [
     	entity: getObjectId("E.Entity.2")
     }
 ];
+var achievements = require('../2-achievements/achievements');
 
-module.exports = users;
+function getRandomName() {
+	let r = Math.random().toString(36).substring(7);
+	return r;
+}
+
+function getAchievementsSample(index) {
+	let n = achievements.length;
+	return _.sample(achievements,_.random(0,n)).map(ach => {return ach._id});
+}
+
+function getRandomDate(from, to) {
+    from = from.getTime();
+    to = to.getTime();
+    return new Date(from + Math.random() * (to - from));
+}
+
+function createRandomUsers() {
+	let ret = [];
+	for (var i = 0; i < maxUsers; i++) {
+		let u = {
+			_id: getObjectId('E.RandomUser.' + i),
+			username: getRandomName(),
+			password: hashPassword('random'),
+			achievements: getAchievementsSample(i),
+			birthday: getRandomDate(new Date('1930-01-01'),new Date('2005-01-01')),
+			createdAt: getRandomDate(new Date('2018-01-01'), new Date('2018-12-31')),
+			lastRequest: getRandomDate(new Date('2018-12-01'),new Date('2018-12-31')),
+			nRequests: _.random(0,5000),
+			userType: 'normal',
+			entity: getObjectId("E.Entity.1")
+		};
+
+		ret.push(u);
+	}
+	return ret;
+}
+
+
+/* Return random users */
+module.exports = users.concat(createRandomUsers());
